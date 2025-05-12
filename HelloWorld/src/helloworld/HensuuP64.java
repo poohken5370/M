8784ca88167eda1abddcfb07293a91bfab4f1888
poohk <poohk@M>
2025-05-12 14:45:42 +0900

    HensuuP64

---
+++ HelloWorld/src/helloworld/HensuuP64.java
@@ -0,0 +1,15 @@
+package helloworld;
+import java.io.*;
+
+public class HensuuP64 {
+	public static void main(String[] args)throws IOException{
+		System.out.println("整数を入力してください。");
+		BufferedReader br=
+				new BufferedReader(new InputStreamReader(System.in));
+		
+		String str=br.readLine();
+		int num=Integer.parseInt(str);
+		System.out.println(num+"が入力されました。");
+	}
+
+}
