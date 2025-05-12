5ebb1d90b28524d2cd8015c8340e00feb6582c73
poohk <poohk@M>
2025-05-12 14:05:28 +0900

    HensuuP53

---
+++ HelloWorld/src/helloworld/HensuuP53.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class HensuuP53 {
+	public static void main(String[] args)
+	{
+		int num;
+		num = 3;
+		System.out.println("変数numの値は" + num + "です。");
+		
+		
+	}
+
+}
