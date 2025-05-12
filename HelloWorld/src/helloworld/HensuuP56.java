551a12ae1bf7df89297f7bc6929d91705820dea8
poohk <poohk@M>
2025-05-12 14:13:16 +0900

    HensuuP56

---
+++ HelloWorld/src/helloworld/HensuuP56.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class HensuuP56 {
+public static void main(String[] args)
+{
+	int num = 3;
+	System.out.println("変数numの値は"+num+"です。");
+	num = 5;
+	System.out.println("変数numの値を変更しました。");
+	System.out.println("変数numの新しい値は"+num+"です。");
+	
+}
+}
