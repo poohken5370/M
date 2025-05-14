6f343abe79742c671799a1fe5a6ac9c086ffc9d2
poohk <poohk@M>
2025-05-14 12:08:52 +0900

    HensuuP69p3

---
+++ HelloWorld/src/helloworld/HensuuP69p3.java
@@ -0,0 +1,14 @@
+package helloworld;
+import java.io.*;
+
+public class HensuuP69p3 {
+	public static void main(String[] args)throws IOException {
+		System.out.println("あなたは何歳ですか？");
+		BufferedReader br=
+				new BufferedReader(new InputStreamReader(System.in));
+		String str=br.readLine();
+		System.out.println("あなたは"+str+"歳です。");
+		
+	}
+
+}
