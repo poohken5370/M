83a87853038baaae95f44f95efe5dc1a43891028
poohk <poohk@M>
2025-05-15 09:41:42 +0900

    EnzanshiP73

---
+++ HelloWorld/src/helloworld/EnzanshiP73.java
@@ -0,0 +1,9 @@
+package helloworld;
+
+public class EnzanshiP73 {
+	public static void main(String[] args) {
+		System.out.println("1+2は"+(1+2)+"です。");
+		System.out.println("3*4は"+(3*4)+"です。");
+	}
+
+}
