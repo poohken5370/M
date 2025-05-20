2612ce728b7a0d12993243bbfbe8ebf9b273e425
poohk <poohk@M>
2025-05-20 09:40:50 +0900

    If文

---
+++ HelloWorld/src/helloworld/IfP123.java
@@ -0,0 +1,25 @@
+package helloworld;
+import java.io.*;
+
+public class IfP123
+{
+	public static void main(String[] args)throws IOException
+	{
+		System.out.println("整数を入力してください。");
+		
+		BufferedReader br=
+				new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		int res = Integer.parseInt(str);
+		
+		if(res == 1)
+		{
+			System.out.println("1が入力されました。");
+		}
+		else {
+			System.out.println("1以外が入力されました。");
+		}
+	}
+
+}
