9cf90399811ae24ec08e33d070b3a9bcd0ec9702
poohk <poohk@M>
2025-05-28 15:35:56 +0900

    追加

---
+++ HelloWorld/src/helloworld/Enzanshi04.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class Enzanshi04 {
+	public static void main(String[] args) {
+		int x=7;
+		x=x*3;
+		System.out.println("xの値は"+x+"です。");
+		
+		System.out.println("xの値は"+(x*0.5)+"です。");		
+	}
+
+}
+
